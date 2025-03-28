1db984e0a4d582952e195e6482e9a058be895ec6
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-28 18:42:26 -0300

    feat: criando componente de letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import styles from "./app.module.css";
 
 import { Tip } from "./components/tip";
+import { Letter } from "./components/letter";
 import { Header } from "./components/header";
 
 function App() {
-  function handleRestartGame() {
-    alert("Reiniciar o jogo!");
-  }
+	function handleRestartGame() {
+		alert("Reiniciar o jogo!");
+	}
 
-  return (
-    <div className={styles.container}>
-      <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
-        <Tip tip="Uma das linguagem de programação mais utilizadas" />
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<main>
+				<Header current={5} max={10} onRestart={handleRestartGame} />
+
+				<Tip tip="Uma das linguagem de programação mais utilizadas" />
+
+				<Letter value="R" />
+				<Letter value="E" />
+				<Letter value="A" />
+				<Letter value="C" />
+				<Letter value="T" />
+			</main>
+		</div>
+	);
 }
 
 export default App;
