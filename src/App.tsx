b65155c9069d60d2af9d1d3901a3be84d29bd8e9
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-31 18:05:34 -0300

    feat: criando handle confirm

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,7 @@ import { LettersUsed, LettersUsedProps } from "./components/letters-used";
 export default function App() {
   const [letter, setLetter] = useState("");
   const [attempts, setAttempts] = useState(0);
-  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([
-    { value: "x", correct: true },
-  ]);
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
   const [challenge, setChallenge] = useState<Challenge | null>(null);
 
   function handleRestartGame() {
@@ -28,6 +26,29 @@ export default function App() {
 
     setChallenge(randomWord);
     setAttempts(0);
+    setLetter("");
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value
+    );
+
+    if (exists) {
+      return alert("Você já utilizou a letra " + value);
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }]);
+
     setLetter("");
   }
 
@@ -55,8 +76,14 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
 
         <LettersUsed data={lettersUsed} />
