fdc03f18cd316542b954f3edc990a36cc5c5bbf0
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-31 00:28:58 -0300

    feat: variações de tamanho de letra

--- src/components/letter/index.tsx
+++ src/components/letter/index.tsx
@@ -1,13 +1,18 @@
 import styles from "./styles.module.css";
 
 type Props = {
-	value?: string;
+  value?: string;
+  size?: "default" | "small";
 };
 
-export function Letter({ value = "" }: Props) {
-	return (
-		<div className={styles.container}>
-			<span>{value}</span>
-		</div>
-	);
+export function Letter({ value = "", size = "default" }: Props) {
+  return (
+    <div
+      className={`${styles.container} ${
+        size === "small" && styles.letterSmall
+      }`}
+    >
+      <span>{value}</span>
+    </div>
+  );
 }
--- src/components/letters-used/index.tsx
+++ src/components/letters-used/index.tsx
@@ -1,9 +1,16 @@
 import styles from "./styles.module.css";
 
+import { Letter } from "../letter";
+
 export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="x" size="small" />
+        <Letter value="r" size="small" />
+      </div>
     </div>
   );
 }
