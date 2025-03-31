9dfefae1483627eb9ae918b3fe0f70f518ec72bb
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-30 23:18:59 -0300

    feat: estilizando main

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,27 @@ import { Letter } from "./components/letter";
 import { Header } from "./components/header";
 
 function App() {
-	function handleRestartGame() {
-		alert("Reiniciar o jogo!");
-	}
+  function handleRestartGame() {
+    alert("Reiniciar o jogo!");
+  }
 
-	return (
-		<div className={styles.container}>
-			<main>
-				<Header current={5} max={10} onRestart={handleRestartGame} />
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
 
-				<Tip tip="Uma das linguagem de programação mais utilizadas" />
+        <Tip tip="Uma das linguagem de programação mais utilizadas" />
 
-				<Letter value="R" />
-				<Letter value="E" />
-				<Letter value="A" />
-				<Letter value="C" />
-				<Letter value="T" />
-			</main>
-		</div>
-	);
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
