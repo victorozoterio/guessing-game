367f8483335cfcfceed3c25c5599facf093de7d9
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-31 23:35:22 -0300

    feat: reiniciando o jogo

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,13 @@ export default function App() {
   const [challenge, setChallenge] = useState<Challenge | null>(null);
 
   function handleRestartGame() {
-    alert("Reiniciar o jogo!");
+    const isConfirmed = window.confirm(
+      "Você tem certeza que deseja reiniciar?"
+    );
+
+    if (isConfirmed) {
+      startGame();
+    }
   }
 
   function startGame() {
